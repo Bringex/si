3998a3186f4247204bd1e27260aa841ee097b059
Андрей Морозов <ezforlive2@gmail.com>
2020-09-12 00:31:42 +0300

    lab_1_task_3

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	int a, b;
-    vector<vector<int>> arr_num;
-	freopen("aplusb.in", "r", stdin);
-	freopen("aplusb.out", "w", stdout);
-	cin >> a >> b;
-	cout << a + b << endl;
+	int h, w, i, j;
+
+	freopen("turtle.in", "r", stdin);
+	freopen("turtle.out", "w", stdout);
+
+	cin >> h >> w;
+	vector<vector<int>> map (h, vector<int>(w));
+	vector<vector<int>> dp_map (h, vector<int>(w));
+
+	for (i = 0; i < h; i++)
+		for (j = 0; j < w; j++)
+			cin >> map[i][j]; 
+
+	dp_map[0][w - 1] = map[0][w - 1];
+	for (i = 1; i < h; i++)
+		dp_map[i][w - 1] = dp_map[i - 1][w - 1] + map[i][w - 1];
+	for (i = w - 2; i >= 0; i--)
+		dp_map[0][i] = dp_map[0][i + 1] + map[0][i];
+
+	for (i = 1; i < h; i++)
+		for (j = w - 2; j >= 0; j--)
+			dp_map[i][j] = max(dp_map[i - 1][j], dp_map[i][j + 1]) + map[i][j];   
+
+	cout << dp_map[h - 1][0];
+
 	return 0;
 }
